4a095ac00bc8f128d0ad4ce50aced1a77579735d
woraphon netnim <woraphoninw01@gmail.com>
2020-03-02 13:26:41 +0700

    create vue cli project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <Header msg="CREATE EMPLOYEE"/>
+    <Table />
+  </div>
+</template>
+
+<script>
+import Table from './components/Table.vue'
+import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Table,
+    Header
+  }
+}
+</script>
+
+<style>
+.container{
+}
+</style>
---
+++ src/components/Table.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <b-table :fields="fields" :items="items" >
+    
+    </b-table>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        fields: [
+          {
+            key: 'name',
+            label: 'Full Name',
+            formatter: 'fullName'
+          },
+        ],
+        items: [
+          { name: { first: 'John', last: 'Doe' }, sex: 'Male', age: 42 },
+          { name: { first: 'Jane', last: 'Doe' }, sex: 'Female', age: 36 },
+          { name: { first: 'Rubin', last: 'Kincade' }, sex: 'male', age: 73 },
+          { name: { first: 'Shirley', last: 'Partridge' }, sex: 'female', age: 62 }
+        ]
+      }
+    },
+    methods: {
+      fullName(value) {
+        return `${value.first} ${value.last}`
+      }
+    }
+  }
+</script>
